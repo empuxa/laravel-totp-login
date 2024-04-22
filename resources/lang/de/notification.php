9b85aa7b0c474db9a72a1cade4a4950847e772a2
Marco Raddatz <marco@empuxa.com>
2024-04-22 11:55:56 +0200

    Fixed translation in email footer

--- resources/lang/de/notification.php
+++ resources/lang/de/notification.php
@@ -2,11 +2,12 @@
 
 return [
     'mail' => [
-        'subject'  => 'Ihr Anmeldecode für :app',
-        'greeting' => 'Hallo :name,',
-        'line-1'   => 'Es gab eine Anmeldeanforderung von :ip. Hier ist Ihr Code, gültig bis :valid_until:',
-        'line-2'   => 'Falls Sie dies nicht waren: Kein Grund zur Panik. Weitere Informationen zu unserem Anmeldeverfahren finden Sie hier: https://google.com',
-        'line-3'   => 'Vielen Dank, dass Sie unsere Dienste nutzen. Bei Fragen stehen wir Ihnen gerne zur Verfügung!',
-        'cta'      => 'Jetzt anmelden',
+        'subject'    => 'Ihr Anmeldecode für :app',
+        'greeting'   => 'Hallo :name,',
+        'line-1'     => 'Es gab eine Anmeldeanforderung von :ip. Hier ist Ihr Code, gültig bis :valid_until:',
+        'line-2'     => 'Falls Sie dies nicht waren: kein Grund zur Panik. Weitere Informationen zu unserem Anmeldeverfahren finden Sie hier: https://google.com',
+        'line-3'     => 'Vielen Dank, dass Sie unsere Dienste nutzen. Bei Fragen stehen wir Ihnen gerne zur Verfügung!',
+        'cta'        => 'Jetzt anmelden',
+        'salutation' => 'Viele Grüße<br>:app-Team',
     ],
 ];
