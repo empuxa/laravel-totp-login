ef6d25400684cd565506f437750b9a7127abe5fa
Marco Raddatz <marco@empuxa.com>
2024-04-22 12:29:22 +0200

    Fixed translation in email footer (#4)

--- resources/lang/en/notification.php
+++ resources/lang/en/notification.php
@@ -2,11 +2,12 @@
 
 return [
     'mail' => [
-        'subject'  => 'Your login code for :app',
-        'greeting' => 'Hello :name,',
-        'line-1'   => 'There was a login request from :ip. Here is your code, which is valid until :valid_until:',
-        'line-2'   => "If it wasn't you: no reason to panic. You can find more information about our login process here: https://google.com",
-        'line-3'   => "Thank you for using our services. If you have any questions, please contact us. We're happy to help!",
-        'cta'      => 'Sign in now',
+        'subject'    => 'Your login code for :app',
+        'greeting'   => 'Hello :name,',
+        'line-1'     => 'There was a login request from :ip. Here is your code, which is valid until :valid_until:',
+        'line-2'     => "If it wasn't you: no reason to panic. You can find more information about our login process here: https://google.com",
+        'line-3'     => "Thank you for using our services. If you have any questions, please contact us. We're happy to help!",
+        'cta'        => 'Sign in now',
+        'salutation' => 'Best regards,<br>:app-Team',
     ],
 ];
