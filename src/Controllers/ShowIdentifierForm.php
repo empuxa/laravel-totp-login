fb043136cd4782efcd0911ccdbe1bf50b56c231d
Marco Raddatz <marco@empuxa.com>
2024-10-25 11:28:12 +0200

    redirect the user if there is an active session

--- src/Controllers/ShowIdentifierForm.php
+++ src/Controllers/ShowIdentifierForm.php
@@ -3,12 +3,17 @@
 namespace Empuxa\TotpLogin\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
 
 class ShowIdentifierForm extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(): RedirectResponse|View
     {
+        if (auth()->check()) {
+            return redirect()->intended(config('totp-login.redirect'));
+        }
+
         return view('totp-login::identifier');
     }
 }
