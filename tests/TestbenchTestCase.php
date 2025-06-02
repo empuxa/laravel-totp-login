0beb582491350f7d69e4aa0edfd88a17bf5bdb68
Marco Raddatz <marco@empuxa.com>
2025-06-02 22:33:42 +0200

    cleanup file

--- tests/TestbenchTestCase.php
+++ tests/TestbenchTestCase.php
@@ -43,28 +43,4 @@ class TestbenchTestCase extends TestCase
 
         $app['config']->set('totp-login.model', User::class);
     }
-
-    /**
-     * @param  array<int|string,mixed>  $params
-     */
-    public function createUser(array $params = []): Model
-    {
-        return config('totp-login.model')::create(array_merge(
-            // Default Laravel params
-            [
-                'name'              => 'Test User',
-                'email'             => 'user@example.com',
-                'email_verified_at' => now(),
-                'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token'    => Str::random(10),
-            ],
-            // Default package params
-            [
-                config('totp-login.columns.code')             => '$2y$10$DJDW1ZCcd.6iqtq/JdivDuWTUCDxVES/efzv1e61CKLhdIJPupzI6', // 123456,
-                config('totp-login.columns.code_valid_until') => now()->addSecond(),
-            ],
-            // Additional test params
-            $params,
-        ));
-    }
 }
